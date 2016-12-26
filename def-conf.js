96391a15c6e8e2590d1a0698dbd3e588408884a3
hezedu@hotmail.com <hezedu@hotmail.com>
2016-12-26 23:13:49 +0800

    init

---
+++ def-conf.js
@@ -0,0 +1,17 @@
+{
+  secure : {
+    ssl : true,
+    sslFilePath: {
+      pfx: '',
+      key: '',
+      cert: '',
+      passphrase: null,
+      ciphers: null,
+      rejectUnauthorized: null,
+      secureProtocol: null,
+      servername:null
+    },
+    sslAutoGenerator: {
+    }
+  }
+}
