b76659bea285984e1cd96b19c81c9c7be187a31f
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-05-28 15:10:07 +0800

    clear test

--- test.js
+++
@@ -1,2 +0,0 @@
-var uid = require('uid-safe');
-console.log(uid.sync(30));
--- test/sock-safe.js
+++
@@ -1,6 +0,0 @@
-var request = require('request');
-
-request.get('http://unix:/opt/linux-remote-data/tmp/u7Riay8sSOtFZokxq3OLuqmj69ER1-eb-dw.sock:/',
-function(err, req, body){
-  console.log(err, body);
-});
