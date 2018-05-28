6d93284727c294f8898285e37f73ec71b447334a
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-05-28 15:11:31 +0800

    clear first-download-ca

--- api/router.js
+++ api/router.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const index = require('./index');
 const login = require('./login');
-const CA = require('./first-download-ca');
-
-router.post('/verifyDownloadCACert', CA.verify);
-router.get('/getDownloadCACertStatus', CA.getStatus);
-router.get('/downloadCACert/:key', CA.download);
 
 router.get('/touch', index.touch);
 
