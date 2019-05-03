ad72cfec4979c9ea7dd51c3e2e1a31bdb71da8b9
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-05-03 14:29:07 +0800

    bak

--- api/user.js
+++ api/user.js
@@ -5,11 +5,16 @@ request.GET = request.get;
 request.POST = request.post;
 request.PUT = request.put;
 request.DELETE = request.delete;
+const { isHaveTerm } = require('../lib/session/user-terms');
 // exports.proxyWebSocket = function(ws, res){
   
 // }
 // use
 exports.proxy = function(req, res){
+  if(!isHaveTerm) {
+    return res.status(403).send('Forbidden'); 
+  }
+
   var unixSocket = 'http://unix:' + util.getTmpName(req.session.id, req.params.username) + '.sock:';
   var x = request[req.method](unixSocket + req.url);
   x.on('error', function(err){
@@ -17,54 +22,6 @@ exports.proxy = function(req, res){
       return res.end('ECONNRESET');
     }
     console.log('user proxy Error: ', err.code);
-
-    let errLogPath = util.getTmpName(req.session.id, req.params.username) + '-err.log';
-
-
-    fs.stat(errLogPath, function(err){
-      if(err){
-        if(err.code === 'ENOENT'){ // 用户正常退出.
-          return res.status(403).send('Forbidden'); 
-        } else {
-          return res.status(500).send('user proxy stat errlog Error: ' + err.code); 
-        }
-      } else {
-        const getErrorLog = (status, message) => {
-          fs.readFile(errLogPath + '.bak', 'utf-8', function(err2, errLog){
-            if(err2){
-              // console.error('user proxy get errlog Error: ', err2);
-              return res.status(500).send('user proxy get errlog Error: ' + err2.code); 
-            }
-            errLog = errLog ? message +
-            '\n\nError log is:\n=================\n' + 
-            errLog + 
-            '\n=================\nYou can report it at: https://github.com/linux-remote/linux-remote/issues'
-              : message;
-            res.status(status).send(errLog);
-          })
-        }
-
-        var loopCount = 0;
-        const loop = () => {
-          loopCount ++;
-          setTimeout(function(){
-            request.get(unixSocket + '/live', function(err){
-              if(!err){
-                getErrorLog(500, 'User process is crash.');
-
-              }else if(loopCount > 5){ 
-                getErrorLog(403, 'User process is died.');
-              }else {
-                loop();
-              }
-            });
-          },500);
-        }
-
-
-        loop();
-      }
-    })
   });
   req.pipe(x);
   x.pipe(res);
