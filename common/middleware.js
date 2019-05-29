48157b4fc56d7bfd39392675d6a48b7aeec92534
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-05-29 19:46:13 +0800

    兼容火狐

--- common/middleware.js
+++ common/middleware.js
@@ -38,7 +38,7 @@ exports.errHandle = function(err, req, res, next) {
   setTimeout(() => {
     if(req.complete){
       res.status(err.status || 500);
-      res.end(`${err.name}: ${err.message}`);
+      res.type('text').end(`${err.name}: ${err.message}`);
     } else {
       // eg: upload , stop immediately
       // console.log('errHandle stop immediately');
