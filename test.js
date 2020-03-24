bd1cc61914e095809d30586bbd81a662212c3b35
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-03-24 08:09:19 +0800

    clear dir

--- test.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer(function(req, res){
-  res.end('hello world!')
-});
-
-server.listen(4002, function(){
-  console.log('server listening ' + 4002);
-});
